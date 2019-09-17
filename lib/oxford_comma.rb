529840416d7c4d4d3cbdb053be3cdcb0cb8a9c04
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:16:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   length = array.length
   new_array = []
   if length = 1
-   new_array = array.join
+    new_array = array.join
   elsif length = 2
     new_array = array.join(" and ")
   end
