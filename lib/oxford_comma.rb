772ea5c62de7be33cf86897299795be88fcee2d8
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:53:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,7 +15,7 @@ def oxford_comma(array)
     new_array = new_array.join
   else
     counter = 0
-    until counter = length - 1
+    until counter == length - 1
       new_array[counter] << (", ")
       counter += 1
     end
