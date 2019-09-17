bacc7170411e668587c3cd4e28085e7641988049
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:21:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
   length = array.length
   new_array = []
-  if length = 1
+  if length == 1
     new_array = array.join
-  elsif length = 2
+  elsif length == 2
     new_array = array.join(" and ")
-  else puts "Length is #{length}."
   end
   new_array
 end
