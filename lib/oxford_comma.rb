363f2f2f5fd8a2f7b2c396f0e1fe3c1eda82c6b7
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:26:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif length == 2
     new_array = array.join(" and ")
   elsif length == 3
-    new_array = array[2]
+    new_array = array.insert()
   end
   new_array
 end
