9b5b849838a0d185ef964d2dcdbe80cae9796304
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:33:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,10 @@ def oxford_comma(array)
   elsif length == 2
     new_array = array.join(" and ")
   elsif length == 3
-    new_array.insert()
-    new_array[2] << ("and ")
+    new_array.insert(0, ", ")
+    new_array.insert(1, ", ")
+    new_array.insert(2, "and ")
+    new_array.join
   end
   new_array
 end
